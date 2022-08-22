c4a879709d8b69f5e7a76c3520e6aa6ba64cdb7c
DevinArdhana <devinardhana0@gmail.com>
2022-08-22 18:03:57 +0800

    first commit

---
+++ bootstrap-5.2.0-beta1-dist/assets/script.js
@@ -0,0 +1,33 @@
+var nilai_un = 80; // number
+nilai_un = 80; // number
+ var nama = "Devin"; //  string
+console.log(nilai_un, nama);
+var penjumlahan =  10;
+var hasil_akhir = nilai_un + penjumlahan;
+var hasil_operasi = (10 + 2) *5;
+
+var nilai = 70;
+if(nilai>=80)
+{
+    console.log("Saya Lulus dengan pujian");
+}
+
+else if(nilai >= 60)
+{
+    console.log("Saya Lulus");
+}
+else if(nilai >= 50)
+{
+    console.log("Saya Lulus Bersyarat");
+}
+
+else{
+    console.log("Saya Tidak Lulus");
+}
+
+
+
+
+
+// + * / -
+// var hasil_akhir_2 = 80 + 10; 
